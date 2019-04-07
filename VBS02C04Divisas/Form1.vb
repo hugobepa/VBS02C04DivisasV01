a6abc9683ba2f0b770a0ead24f3af9897adbf61e
hugo bermudez <hugo_bcn@hotmail.com>
2019-04-07 21:29:11 +0200

    V02 while

--- VBS02C04Divisas/Form1.vb
+++ VBS02C04Divisas/Form1.vb
@@ -7,18 +7,30 @@
         Dim cambio As Double
         Dim centimos = New Integer() {50, 20, 10, 5, 2, 1}
         Dim monedas(5) As Integer
+        Dim index As Integer
+        Dim txt As String
+
         'students(0)'
 
         cambio = CDbl(TXTEuros.Text.Trim) * 100
 
         'calculos'
 
-        For index = 0 To 5
+        While 0 < cambio
+
             monedas(index) = Int(cambio / centimos(index))
             cambio = cambio - (centimos(index) * monedas(index))
-        Next
+            index = index + 1
+        End While
 
-        'mostrar'
+        'For index As Integer = 1 To 5'
+        'monedas(index) = Int(cambio / centimos(index))
+        'cambio = cambio - (centimos(index) * monedas(index))
+        'Next 
+
+
+
+
         TXT50.Text = monedas(0)
         TXT20.Text = monedas(1)
         TXT10.Text = monedas(2)
@@ -27,9 +39,14 @@
         TXT1.Text = monedas(5)
 
 
+
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TXTEuros_TextChanged(sender As Object, e As EventArgs) Handles TXTEuros.TextChanged
 
     End Sub
 End Class
